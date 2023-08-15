1d3f031fc5ca61ac007a60dcfbdfad0bb554d2ae
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2023-08-15 11:24:13 -0300

    estilo footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,12 @@ import { FaInstagram, FaYoutube, FaSpotify, FaDeezer } from 'react-icons/fa';
 
 function Footer() {
     return (
-        <footer className="absolute bottom-0 left-0 right-0 p-4 flex justify-between items-center bg-opacity-50 bg-black">
+        <footer className="absolute bottom-0 p-3 left-0 right-0 flex justify-between items-center bg-opacity-50 bg-black">
             <div className="flex space-x-4 text-white">
-                <a href="https://www.instagram.com/iarabicalhocantora/"><FaInstagram /></a>
-                <a href="https://www.youtube.com/@iarabicalhocantora"><FaYoutube /></a>
-                <a href="https://open.spotify.com/intl-pt/artist/0MDMFoY2RXR6J3xD5F4KCs"><FaSpotify /></a>
-                <a href="https://www.deezer.com/br/artist/183519917"><FaDeezer /></a>
+                <a href="https://www.instagram.com/iarabicalhocantora/"><FaInstagram size={25}/></a>
+                <a href="https://www.youtube.com/@iarabicalhocantora"><FaYoutube size={25}/></a>
+                <a href="https://open.spotify.com/intl-pt/artist/0MDMFoY2RXR6J3xD5F4KCs"><FaSpotify size={25}/></a>
+                <a href="https://www.deezer.com/br/artist/183519917"><FaDeezer size={25}/></a>
             </div>
             <div>
                 <p className="text-white invisible md:visible">Todos os direitos reservados - Light FOR 2023</p>
